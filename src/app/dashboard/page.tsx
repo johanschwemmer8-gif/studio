65d008b3eafb80446779e91bab021b15f428b767
Johan Schwemmer <johanschwemmer8@gmail.com>
2025-09-08 10:23:25 +0000

    I get this on my screen404 This page could not be found.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function DashboardRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/dashboard/admin');
+  }, [router]);
+
+  return null; 
+}

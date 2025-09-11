1e712eabb027a317b747b1995bdcd1692396c06c
Johan Schwemmer <johanschwemmer8@gmail.com>
2025-09-11 15:19:45 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/components/dashboard/brand-qr-template-designer.tsx
+++ src/components/dashboard/brand-qr-template-designer.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';

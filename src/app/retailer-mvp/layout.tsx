5aef4b0c82313bc76ac8effca0010a103d30e325
Johan Schwemmer <johanschwemmer8@gmail.com>
2025-09-12 14:38:24 +0000

    Reamove the image behind the word "...Africa"

--- src/app/retailer-mvp/layout.tsx
+++ src/app/retailer-mvp/layout.tsx
@@ -40,14 +40,6 @@ export default function RetailerMvpLayout({
          <footer className="p-4 text-center text-xs text-muted-foreground border-t">
             <div className="flex items-center justify-center gap-2">
                 <span>Powered by iNteract AOE. Made in South Africa.</span>
-                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" viewBox="0 0 900 600">
-                    <rect width="900" height="600" fill="#DE3831"/>
-                    <rect width="900" height="300" y="300" fill="#002395"/>
-                    <path d="M0 0L450 300L0 600V0Z" fill="#007A4D"/>
-                    <path d="M-10 0L440 300L-10 600a120 120 0 0 1 0-600z" fill="#FFB612"/>
-                    <path d="M-20 0L430 300L-20 600a120 120 0 0 1 0-600z" fill="#fff"/>
-                    <path d="M0 120L300 300L0 480V120Z" fill="#000"/>
-                </svg>
             </div>
         </footer>
       </SidebarInset>

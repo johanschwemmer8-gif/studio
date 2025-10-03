b9352518b1642be94a7376c674df63f7cdc147a3
Johan Schwemmer <johanschwemmer8@gmail.com>
2025-10-03 07:32:27 +0000

    I apologize,I mean that the enitre background color must be changed to m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="light">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`} suppressHydrationWarning>
         {children}
         <Toaster />

ce5b45f3fe79631814868bfa14e37e97922c7e2b
Johan Schwemmer <johanschwemmer8@gmail.com>
2025-10-03 07:28:26 +0000

    I think we should perhaps change the background colour to a softer light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="light">
       <body className={`${inter.className} antialiased`} suppressHydrationWarning>
         {children}
         <Toaster />

d892256f374332f0f2b6556ba63eb3e1322ac0dc
Johan Schwemmer <johanschwemmer8@gmail.com>
2026-04-16 08:13:49 +0000

    I treid to re-publish the MVP but it failed: Failed to run the framework

--- next.config.js
+++ next.config.js
@@ -8,12 +8,6 @@ const nextConfig = {
     },
   },
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {

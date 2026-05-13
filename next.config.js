04d2d7556eb4878c085f3d10f84b81d618f3fa90
Johan Schwemmer <johanschwemmer8@gmail.com>
2026-05-13 11:49:41 +0000

    Next.js (14.2.35) is outdated. Please update Next.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb', // Default is 1mb
-      // Increase timeout for potentially long-running AI operations
-      executionTimeout: 120, // 120 seconds
-    },
-  },
   /* config options here */
   images: {
     remotePatterns: [
